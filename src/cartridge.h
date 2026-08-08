3a0bdfaf6cb350723a7d1d7f26d2d3e281656396
examp2026 <examp2026@gmail.com>
2026-08-08 14:27:12 +0500

    style/docs: clean up code formatting, parameter names and updated dev notes

--- src/cartridge.h
+++ src/cartridge.h
@@ -8,7 +8,7 @@
 class Cartridge
 {
 public:
-    Cartridge(Sram& memory, std::vector<uint8_t> raw_data);
+    Cartridge(Sram& sram, std::vector<uint8_t> raw_data);
     uint8_t read(uint16_t address);
     void write(uint16_t address, uint8_t value);
 private:    
